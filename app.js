866773bc85d04dd9d0cd7cf022790735d71f3e31
ignacio-r <ignaciorobledounq@gmail.com>
2023-12-08 07:58:10 -0300

    create custom web components

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ import { loadData } from './services/Menu.js';
 import Router from './services/Router.js';
 
 
+//Link my web components
+import MenuPage from './components/MenuPage.js';
+import DetailsPage from './components/DetailsPage.js';
+import OrderPage from './components/OrderPage.js';
+
 window.app = {}
 app.store = Store;
 app.router = Router;
---
+++ components/MenuPage.js
@@ -0,0 +1,8 @@
+class  MenuPage extends HTMLElement {
+    constructor() {
+        super();
+    }
+}
+customElements.define("menu-page", MenuPage);
+
+export default MenuPage
